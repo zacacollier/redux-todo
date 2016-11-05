461d7edd53cf2dc638a38dce01ce21498c12ed9b
zacacollier <zacacollier@gmail.com>
2016-11-05 12:57:59 -0500

    create store in index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+import { createStore } from 'redux'
+import toDoApp from './reducers.js'
+
+let store = createStore(toDoApp)
--- reducers.js
+++ reducers.js
@@ -1,4 +1,4 @@
-import { VisibilityFilters } from './actions'
+import { combineReducers } from 'redux'
 import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
 const { SHOW_ALL } = VisibilityFilters
 
@@ -7,6 +7,15 @@ const initialState = {
     todos: []
 }
 
+
+function visibilityFilter(state = SHOW_ALL, action) {
+    switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+        return action.filter
+    default:
+        return state
+    }
+}
 
 function todos(state = [], action) {
     switch(action.type) {
@@ -31,35 +40,10 @@ function todos(state = [], action) {
             return state
     }
 }
-function visibilityFilter(state = SHOW_ALL, action) {
-    switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            return action.filter
-        default:
-            return state
-    }
-}
-function toDoApp(state = initialState, action) {
-    switch (action.type) {
-        case SET_VISIBILITY_FILTER:
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            })
-        case ADD_TODO:
-            return state.map((todo, index) => {
-                return [
-                    ...state,
-                    {
-                        text: action.text,
-                        completed: false
-                    }
-                ]
-            })
-        case TOGGLE_TODO:
-            return Object.assign({}, state, {
-                todos: todos(state.todos, action)
-            })
-        default:
-            return state
-    }
-}
+
+const toDoApp = combineReducers({
+    visibilityFilter,
+    todos
+})
+
+export default toDoApp
